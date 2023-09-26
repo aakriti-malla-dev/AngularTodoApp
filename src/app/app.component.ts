c696dff84545b61b997ef064e53e73db240b4420
Aakriti Malla <aakriti.malla@gojokotech.com>
2023-09-26 16:56:37 +0530

    Add Remove Func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todomvc';
+
+  allItems: any[] = [];
+
+  @Input()
+  newTodoText = '';
+
+  get items() {
+      if (this.filter === 'all') {
+        return this.allItems;
+      }
+
+      return this.allItems.filter((item) => this.filter === 'completed' ? item.completed : !item.completed);
+  }
+
+  get remaining() {
+    return this.allItems.filter(todo => !todo.completed).length;
+  }
+
+  filter: 'all' | 'active' | 'completed' = 'all';
+
+  toggleCompletion(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  toggleAll() {
+    this.allItems.forEach(todo => todo.completed = !todo.completed);
+  }
+
+  get completed() {
+    return this.allItems.filter(todo => todo.completed);
+  }
+
+  removeCompleted() {
+    this.allItems = this.allItems.filter(todo => !todo.completed);
+  }
+
+  addTodo() {
+    if (this.newTodoText.trim().length) {
+        this.allItems.push({ id: Date.now(), title: this.newTodoText, completed: false, editing: false });
+        this.newTodoText = '';
+    }
+  }
+
+  removeTodo(todo){
+    this.allItems.splice(this.allItems.indexOf(todo), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // THIS LINE OF CODE
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule // AND ITS RESPECTIVE IMPORT
   ],
   providers: [],
   bootstrap: [AppComponent]
