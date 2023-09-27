b64e0e00dca69469bdc222bd4ac5dfc3a66070b5
Aakriti Malla <aakriti.malla@gojokotech.com>
2023-09-27 11:07:37 +0530

    Refactoring

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
   @Input() allItems: any;
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
 })
 export class ItemComponent {
     @Input() item: any;
